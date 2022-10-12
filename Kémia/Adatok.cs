354837c0a1931d58a36de01138b0cdb5e3781b66
cisco <cisco@DESKTOP-D4D1G54>
2022-10-12 11:55:19 +0200

    Add project files.

---
+++ Kémia/Adatok.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kémia
+{
+    class Adatok
+    {
+        //2. feladat
+        string ev;
+        string elem;
+        string vegyjel;
+        int rendszam;
+        string felfedezo;
+        
+
+        public string Ev { get => ev; set => ev = value; }
+        public string Elem { get => elem; set => elem = value; }
+        public string Vegyjel { get => vegyjel; set => vegyjel = value; }
+        public int Rendszam { get => rendszam; set => rendszam = value; }
+        public string Felfedezo { get => felfedezo; set => felfedezo = value; }
+       
+
+        public Adatok(string ev, string elem, string vegyjel, int rendszam, string felfedezo)
+        {
+            Ev = ev;
+            Elem = elem;
+            Vegyjel = vegyjel;
+            Rendszam = rendszam;
+            Felfedezo = felfedezo;
+        }
+        public Adatok(string adatsor)
+        {
+            string[] adatok = adatsor.Split(';');
+            
+            Ev = adatok[0];
+            Elem = adatok[1];
+            Vegyjel = adatok[2];
+            Rendszam = int.Parse(adatok[3]);
+            Felfedezo = adatok[4];
+        }
+    }
+}
---
+++ Kémia/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Kémia
+{
+    class Program
+    {
+        public static List<Adatok> adatok = new List<Adatok>();
+        static void Main(string[] args)
+        {
+            
+
+            using (StreamReader file = new StreamReader("felfedezesek.csv" ))            //2.feladat beolvasas
+            {
+                file.ReadLine();
+                while (!file.EndOfStream)
+                {
+                    adatok.Add(new Adatok(file.ReadLine()));
+                }
+                file.Close();
+            }
+            //3.feladat Hány kémia elem van az állományban
+            Console.WriteLine($"3.feladat: Elemek száma: {adatok.Count}");
+            //4.feladat felfedezett kemiai elemek az ókorban          
+            Console.WriteLine($"4.feladat: Felfedezések száma az ókorban: {adatok.FindAll(x => x.Ev == "Ókor").Count}");
+            //5./6. feladat
+            feladat6();
+
+            feladat8();
+
+          
+            Console.ReadKey();
+      
+        }
+
+        private static void feladat8()
+        {
+            Console.WriteLine("8.feladat: Statitzika");
+            adatok.GroupBy(a => a.Ev).Where(c =>c.Count() > 3 && c.Key != "Ókor").ToList().ForEach(g => Console.WriteLine($"\t{g.Key}: {g.Count()} db"));
+        }
+
+        private static void feladat6()
+        {
+            string kell = feladat5();
+            Console.WriteLine($"6.feladat: Keresés");
+            Adatok van = adatok.Find(x => x.Vegyjel.ToLower() == kell.ToLower());
+            if (! (van is null))
+            {
+                Console.WriteLine($"\tAz elem vegyjele: {van.Vegyjel} \n\tAz elem neve: {van.Elem} \n\tRendszám: {van.Rendszam} \n\tFelfedezés éve: {van.Ev} \n\tFelfedező: {van.Felfedezo}");
+            }
+        }
+
+        private static string feladat5()
+        {
+            //5.feladat
+            Console.Write($"5.feladat: Kérek egy vegyjelet:");
+            string vegyjel2 = Console.ReadLine();
+            
+
+            while (!Regex.IsMatch(vegyjel2,@"^[a-zA-Z]{1,2}$"))
+            {
+                Console.Write($"5.feladat: Kérek egy vegyjelet:");
+                vegyjel2 = Console.ReadLine();                     
+            }
+            return vegyjel2;
+        }
+    }
+}
